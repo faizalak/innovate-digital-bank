c08c901be4beedb5333fc30d557a4c66e5c14c7c
Faizal Abdul Khader <faizal.khader@redhat.com>
2021-09-28 21:12:40 -0500

    changed mongoose connect #12

--- accounts/server.js
+++ accounts/server.js
@@ -9,7 +9,16 @@ var port = 3400;
 
 console.log(`Running on ${process.env.BASE_PATH}:${port}, connecting to ${process.env.MONGO_URL}`)
 
-mongoose.connect('${process.env.MONGO_URL}');//function (ignore, connection) {
+console.log('MongoDB URL in use: ' + process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection
+ .once('open', () => console.log('connected to MongoDB!'))
+ .on('error', err => console.error('connecting to MongoDB ' + err))
+
+console.log('Express server listening on port ' + port) 
+
+//mongoose.connect('${process.env.MONGO_URL}', 
+//function (ignore, connection) {
     //connection.onOpen();
     //server.listen(port, function () {
      //   console.log('Server running on port: %d', port);
