d4558fb9bf65e4b5d1b464e2c6c7c3efdfb6b060
Faizal Abdul Khader <faizal.khader@redhat.com>
2021-09-28 21:02:27 -0500

    changed mongoose connect #10

--- accounts/server.js
+++ accounts/server.js
@@ -9,14 +9,7 @@ var port = 3400;
 
 console.log(`Running on ${process.env.BASE_PATH}:${port}, connecting to ${process.env.MONGO_URL}`)
 
-const options = {
-        autoIndex: false, // Don't build indexes
-        poolSize: 10, // Maintain up to 10 socket connections
-        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-        family: 4 // Use IPv4, skip trying IPv6
-};
-mongoose.connect('${process.env.MONGO_URL}',options);//function (ignore, connection) {
+mongoose.connect('${process.env.MONGO_URL}');//function (ignore, connection) {
     //connection.onOpen();
     //server.listen(port, function () {
      //   console.log('Server running on port: %d', port);
