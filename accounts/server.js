56511b34cea023b762f7bef1325e9d7c73093c00
Faizal Abdul Khader <faizal.khader@redhat.com>
2021-09-28 20:45:02 -0500

    changed mongoose connect #8

--- accounts/server.js
+++ accounts/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { use } = require('chai');
 const mongoose = require('mongoose');
 require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 
@@ -8,9 +9,16 @@ var port = 3400;
 
 console.log(`Running on ${process.env.BASE_PATH}:${port}, connecting to ${process.env.MONGO_URL}`)
 
-mongoose.connect('${process.env.MONGO_URL}',  {
-    serverSelectionTimeoutMS: 5000
-  }).catch(err => console.log(err.reason));//function (ignore, connection) {
+const options = {
+        user=use,
+        pass=password,
+        autoIndex: false, // Don't build indexes
+        poolSize: 10, // Maintain up to 10 socket connections
+        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect('${process.env.MONGO_URL}',options);//function (ignore, connection) {
     //connection.onOpen();
     //server.listen(port, function () {
      //   console.log('Server running on port: %d', port);
