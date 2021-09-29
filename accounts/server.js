222b8f45c76e02169ce2dc21bc6d7d7f2ad8a5ad
Faizal Abdul Khader <faizal.khader@redhat.com>
2021-09-29 16:09:12 -0500

    changed mongoose connect #22

--- accounts/server.js
+++ accounts/server.js
@@ -19,28 +19,25 @@ const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
 console.log(`Running on ${process.env.BASE_PATH}:${port}, connecting to ${process.env.MONGO_URL} and mongo host `+MONGODB_REPLICA_HOSTNAMES)
 
 // connection to mongodb
-//mongoose
- //   .connect('mongodb://' + MONGODB_REPLICA_HOSTNAMES + '/', {
-  //      user: MONGODB_USER,
-  //      pass: MONGODB_PASSWORD,
-  //      dbName: MONGODB_DBNAME,
+mongoose
+    .connect('mongodb://' + MONGODB_REPLICA_HOSTNAMES + '/', {
+        user: MONGODB_USER,
+        pass: MONGODB_PASSWORD,
+        dbName: MONGODB_DBNAME,
   //      replicaSet: MONGODB_REPLICA_SET,
   //      authSource: MONGODB_AUTH_DBNAME,
   //      tls: false,
   //      tlsCAFile: MONGODB_CA_PATH,
   //      useCreateIndex: true,
    //     useNewUrlParser: true,
-   //     useUnifiedTopology: true})
-  //  .catch(error => {
-  //      console.log(error)
-  //      process.exit(1)
-  //  });
-
-mongoose.connect('${process.env.MONGO_URL}')
+   //     useUnifiedTopology: true
+            })
     .catch(error => {
-              console.log(error)
-              process.exit(1)
-          });
+        console.log(error)
+        process.exit(1)
+    });
+
+//mongoose.connect('${process.env.MONGO_URL}', {}
 //function (ignore, connection) {
     //connection.onOpen();
     //server.listen(port, function () {
